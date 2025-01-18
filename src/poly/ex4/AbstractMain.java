53525f704797da3bc56997f1ef2739fa8f869820
drnomader <drnomader@gmail.com>
2025-01-18 23:04:55 +0900

    Add poly.ex4 package

---
+++ src/poly/ex4/AbstractMain.java
@@ -0,0 +1,32 @@
+package poly.ex4;
+
+public class AbstractMain {
+	public static void main(String[] args) {
+		//unable to initialize abstract class
+		//AbstractAnimal abstractAnimal = new AbstractAnimal();
+
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		Cow cow = new Cow();
+
+		soundAnimal(dog);
+		soundAnimal(cat);
+		soundAnimal(cow);
+
+		moveAnimal(dog);
+	}
+
+	//unchanging code
+	private static void soundAnimal(AbstractAnimal animal) {
+		System.out.println("동물 소리 테스트 시작");
+		animal.sound();
+		System.out.println("동물 소리 테스트 종료");
+	}
+
+	//unchanging code
+	private static void moveAnimal(AbstractAnimal animal) {
+		System.out.println("동물 이동 테스트 시작");
+		animal.move();
+		System.out.println("동물 이동 테스트 종료");
+	}
+}
